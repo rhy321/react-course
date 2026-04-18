7843657a73c88d63d0d4306b5f7f2f8db34df58a
rhy321 <rhyahm25@gmail.com>
2026-04-18 13:53:28 +0530

    make create order button work

--- ecommerce/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce/src/pages/checkout/CheckoutPage.jsx
@@ -38,7 +38,7 @@ return (
       <div className="checkout-grid">
         <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
 
-        <PaymentSummary paymentSummary={paymentSummary} />
+        <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
       </div>
     </div>
   </>
--- ecommerce/src/pages/checkout/PaymentSummary.jsx
+++ ecommerce/src/pages/checkout/PaymentSummary.jsx
@@ -1,6 +1,17 @@
-import {formatMoney} from "../../utils/money.js"
+import axios from "axios";
+import { useNavigate } from "react-router";
+import {formatMoney} from "../../utils/money.js";
 
-export function PaymentSummary({paymentSummary}) {
+export function PaymentSummary({paymentSummary, loadCart}) {
+
+  const navigate = useNavigate();
+
+  const createOrder = async() => {
+    await axios.post('/api/orders');
+    await loadCart();
+    navigate('/orders');
+  }
+
   return (
     <>
       <div className="payment-summary">
@@ -35,7 +46,8 @@ export function PaymentSummary({paymentSummary}) {
               <div className="payment-summary-money">{formatMoney(paymentSummary.totalCostCents)}</div>
             </div>
 
-            <button className="place-order-button button-primary">
+            <button className="place-order-button button-primary"
+              onClick={createOrder}>
               Place your order
             </button>
           </>
